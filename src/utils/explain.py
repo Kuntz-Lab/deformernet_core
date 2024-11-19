890081bd2d381b5d0bb9358286dd6870c9b245a6
Britton <brittonty@gmail.com>
2024-11-19 15:40:51 -0700

    c

--- src/utils/explain.py
+++ src/utils/explain.py
@@ -1,7 +1,28 @@
 import matplotlib.pyplot as plt 
+import numpy as np
+from torch import Tensor
+import open3d
+from point_cloud_utils import pcd_ize
 
 def plot_points():
     print("hello")
+
+def visualize_pointclouds_simple_from_tensor(point_cloud: Tensor, plot_origin: bool = True):
+    """
+    
+    """
+    items = []
+
+    pc1_array = point_cloud[0].cpu().numpy()
+
+    pc1 = pcd_ize(pc1_array, color=[0, 0, 0])
+    items.append(pc1)
+
+    if plot_origin:
+        coor = open3d.geometry.TriangleMesh.create_coordinate_frame(size=0.05) 
+        items.append(coor)
+    
+    open3d.visualization.draw_geometries(items) # remains blocked here until visualization window is closed
 
 def visualize_weights(model):
     for name, param in model.named_parameters():
